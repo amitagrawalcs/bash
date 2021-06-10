064bc34dfda33394bc6650f57f0e020f39143328
Amit Agrawal <amitsite03@gmail.com>
2021-06-10 16:50:47 +0530

    Initial Commit

--- project_1/ten.sh
+++ project_1/ten.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-
+# date in format %Y-%m-%d
 start_ts=$(date -d $1 '+%s')
 end_ts=$(date -d $2 '+%s')
 echo $(( ( end_ts - start_ts )/(60*60*24) ))
--- project_1/while.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-   
-value=$1
-index=10
-while [ $index -ge 1 ]
-do
-	echo $(( value*index ))
-    	index=$(( index-1 ))
-done
